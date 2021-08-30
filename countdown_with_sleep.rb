828c367981bebc3a9185f1f32229a19dd21f1416
EveOfMaly <john.a.kyeremeh@gmail.com>
2021-08-30 09:28:44 -0700

    Completed Assignment

--- countdown_with_sleep.rb
+++ countdown_with_sleep.rb
@@ -2,10 +2,11 @@ require 'pry'
 
 #write your code here
 def countdown_with_sleep(number)
-    while number != 0
+    while number > 0
      puts "#{number} SECOND(S)!"
-     number -= 1 
      sleep(1)
+     number -= 1 
+  
     end
     "HAPPY NEW YEAR!"
 end
